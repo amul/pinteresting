a86d427e79d4d736aeda57b1cfd3932738e2b146
Amul Patel <amul84@yahoo.com>
2013-10-19 19:04:31 -0700

    added pages and routed root to it

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Pinteresting::Application.routes.draw do
+  root 'pages#home'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-
+  
+  
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
